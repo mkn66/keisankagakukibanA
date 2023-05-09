a8207fca3a3119f3a43966517447bb72c97d574b
mkn66 <93748591+mkn66@users.noreply.github.com>
2023-05-09 17:00:21 +0900

    Update enshu_2_24.f90

--- enshu_2_24.f90
+++ enshu_2_24.f90
@@ -5,19 +5,15 @@ program main
     call random_seed
     call random_number(a(1:3))
     call random_number(b(1:3))
-    c(1,:) = a(:)
-    c(2,:) = b(:)
-    write(*, '(3e10.4)') a
-    write(*, '(3e10.4)') b
+    c(1,1:3) = a(1:3)
+    c(2,1:3) = b(1:3)
+
     
     do i = 1, 3
-        write(*, '(3e10.4)') c(1:2, 1:3)
         c(1:2, 1:3) =  cshift(c,i-1, 2)
-        write(*,*) ""
-        write(*, '(3e10.4)') c(1:2, 1:3)
+
         bu(1:2, 1:2) = c(1:2, 2:3)
-        write(*,*) ""
-        write(*, '(2e10.4)') bu(1:2, 1:2)
+
         det = bu(1,1)*bu(2,2) - bu(1,2)*bu(2,1)
         ab(i) = det
     enddo
